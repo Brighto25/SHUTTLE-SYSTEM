c8baae10ea090286fbdc42124da3f5bc37044413
Brighto25 <Bright.Addaneyf3@gmail.com>
2026-05-15 20:03:02 +0000

    edit phone input

--- shuttle-system/shuttle-project/src/components/payment/PhoneInput.jsx
+++ shuttle-system/shuttle-project/src/components/payment/PhoneInput.jsx
@@ -1,16 +1,22 @@
 function PhoneInput({ value, onChange }) {
+  const handleChange = (e) => {
+    const cleaned = e.target.value.replace(/\D/g, "");
+    onChange(cleaned);
+  };
+
   return (
     <div className="pay-num-input">
-      <span id="pay-prefix">+233</span>
+      <span>+233</span>
       <input
         type="tel"
         placeholder="24 000 0000"
         maxLength={10}
         value={value}
-        onChange={e => onChange(e.target.value)}
+        onChange={handleChange}
+        inputMode="numeric"
       />
     </div>
-  )
+  );
 }
 
-export default PhoneInput
+export default PhoneInput;
